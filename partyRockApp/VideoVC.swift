2ef7064c4f3f71585b7ecf161fb1ebb029c3dfe6
marvin evins <maevins@cps.edu>
2016-08-12 19:02:19 -0500

    showing youtube videos and the rest of setup

--- partyRockApp/VideoVC.swift
+++ partyRockApp/VideoVC.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class VideoVC: UITableViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet var titleLbl: UILabel!
+    private var _partyRock: PartyRock!
+    
+    var partyRock: PartyRock {
+        get {
+            return _partyRock
+        } set {
+            _partyRock = newValue
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLbl.text = partyRock.videoTitle
+        webView.loadHTMLString(partyRock.videoURL, baseURL: nil)
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
